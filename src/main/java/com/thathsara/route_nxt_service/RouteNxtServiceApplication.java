916a93bf8dbcf280871c3559a4706b3106eb31ab
ThathsaraDV <thathsaradananjaya@gmail.com>
2024-06-23 23:36:22 +0530

    initialized project.

---
+++ src/main/java/com/thathsara/route_nxt_service/RouteNxtServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thathsara.route_nxt_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteNxtServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RouteNxtServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thathsara/route_nxt_service/RouteNxtServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thathsara.route_nxt_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteNxtServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
